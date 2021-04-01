e918abb4b0e46b471d6e7192c6d85bb6005433b7
Sayem <sayemkhan435@gmail.com>
2021-04-01 17:04:57 +0530

    first commit

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ mongoose.connection.on('error',(err)=>{
     }
 })
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, ()=>{
     console.log("Server is listening on port number : " +port)
 })
